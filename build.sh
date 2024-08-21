2a223fe1595c6d32de5f00dc1478bbd7c933f0c6
Fred Bunt <fredrick.bunt@gmail.com>
2024-08-21 01:17:39 -0600

    Set upstream and add packages

--- build.sh
+++ build.sh
@@ -11,13 +11,7 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+rpm-ostree install \
+    gparted \
+    mediawriter \
+    nerd-fonts
