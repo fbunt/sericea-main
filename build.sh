9d4852c0f8c3b8d6cbd8b0fe4e309cddfbb1300f
Fred Bunt <fredrick.bunt@gmail.com>
2025-08-19 15:49:55 -0600

    Add keychain

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 rpm-ostree install \
     gparted \
+    keychain \
     mediawriter \
     neovim \
     powertop
