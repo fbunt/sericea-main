d6eedaac5b6c375b650ed58642084834c65fb905
Fred Bunt <fredrick.bunt@gmail.com>
2026-03-14 02:22:02 -0600

    Try adding docker

--- build.sh
+++ build.sh
@@ -12,6 +12,10 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 rpm-ostree install \
+    containerd \
+    docker-cli \
+    docker-compose \
+    docker-compose-switch \
     gparted \
     keychain \
     libvirt \
