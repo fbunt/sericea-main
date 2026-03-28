eb41c9057e211fc21e379785e849bfe9b4ee742a
Fred Bunt <fredrick.bunt@gmail.com>
2026-03-28 00:41:18 -0600

    Add dev tools

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 rpm-ostree install \
+    @development-tools \
     containerd \
     docker-cli \
     docker-compose \
